a7cebe1300d4077a0dfdb61c4a2bd8efe265f7d9
Travis Smith <travisws@uci.edu>
2014-10-26 13:49:54 -0700

    Initial Commit

---
+++ Editor/Engine.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+
+namespace Editor
+{
+  class Command
+  {
+    public enum Type { NONE, MAP_LEFT_CLICK, MAP_RIGHT_CLICK };
+
+    static public Command CreateMapClickCmd(Point loc, bool leftClick = true)
+    {
+      Command cmd = new Command();
+      cmd.m_type = leftClick ? Type.MAP_LEFT_CLICK : Type.MAP_RIGHT_CLICK;
+      cmd.m_props.Add((int)loc.X);
+      cmd.m_props.Add((int)loc.Y);
+      return cmd;
+    }
+
+    private Command()
+    {
+      m_type = Type.NONE;
+      m_props = new List<int>(0);
+      m_tags = new List<string>(0);
+    }
+
+    public Type m_type;
+    public List<int> m_props;
+    public List<string> m_tags;
+  }
+
+  class Engine
+  {
+    public delegate void UpdateSysMsgText(string msg);
+
+    public Engine(MainWindow mainWindow)
+    {
+      m_mainWindow = mainWindow;
+      m_running = false;
+      m_queueLock = new object();
+      m_queueThread = new Thread(Run);
+      m_queueThread.Name = "Engine Queue Thread";
+      m_cmdQueue = new Queue<Command>(0);
+    }
+
+    ~Engine()
+    {
+      Stop();
+    }
+
+    public void Run()
+    {
+      lock (m_queueLock)
+      {
+        while (m_running)
+        {
+          Monitor.Wait(m_queueLock);
+          ProcessCommands();
+        }
+      }
+    }
+
+    public void Start()
+    {
+      m_running = true;
+      m_queueThread.Start();
+    }
+
+    public void Stop()
+    {
+      lock (m_queueLock)  
+      {
+        m_running = false;
+        Monitor.Pulse(m_queueLock);
+      }
+
+      m_queueThread.Join();
+    }
+
+    public void AddCommand(Command cmd)
+    {
+      lock (m_queueLock)
+      {
+        m_cmdQueue.Enqueue(cmd);
+        Monitor.Pulse(m_queueLock);
+      }
+    }
+
+    private Engine(ref Engine original) { }
+
+    private void ProcessCommands()
+    {
+      while(m_cmdQueue.Count() > 0)
+      {
+        Command cmd = m_cmdQueue.Dequeue();
+
+        switch (cmd.m_type)
+        {
+          case Command.Type.NONE:
+            break;
+          case Command.Type.MAP_LEFT_CLICK:
+            MapLeftClicked(cmd.m_props[0], cmd.m_props[1]);
+            break;
+          case Command.Type.MAP_RIGHT_CLICK:
+            MapRightClicked(cmd.m_props[0], cmd.m_props[1]);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    private void MapLeftClicked(int xLoc, int yLoc)
+    {
+      int xCord = xLoc / 64;
+      int yCord = yLoc / 64;
+      string msg = String.Format("Map Left Clicked at ({0}, {1}) grid evaluates to ({2}, {3}).", xLoc, yLoc, xCord, yCord);
+      m_mainWindow.Dispatcher.InvokeAsync((Action)delegate() { m_mainWindow.UpdateSysMsg(msg); });
+    }
+
+    private void MapRightClicked(int xLoc, int yLoc)
+    {
+      int xCord = xLoc / 64;
+      int yCord = yLoc / 64;
+      string msg = String.Format("Map Right Clicked at ({0}, {1}) grid evaluates to ({2}, {3}).", xLoc, yLoc, xCord, yCord);
+      m_mainWindow.Dispatcher.InvokeAsync((Action)delegate() { m_mainWindow.UpdateSysMsg(msg); });
+    }
+
+    private MainWindow m_mainWindow;
+    private bool m_running;
+    private object m_queueLock;
+    private Thread m_queueThread;
+    private Queue<Command> m_cmdQueue;
+  }
+}
---
+++ Editor/MainWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Editor
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Engine m_mainEngine;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      m_mainEngine = new Engine(this);
+      m_mainEngine.Start();
+
+      // Map Draw Test Code -----------------------------------------------------------------------
+      int cols = 20;
+      int rows = 10;
+      int gridSize = 64;
+
+      MapCanvas.Height = rows * gridSize;
+      MapCanvas.Width = cols * gridSize;
+      GeometryGroup gridSquares = new GeometryGroup();
+
+      for(int rIndex = 0; rIndex < rows; ++rIndex)
+      {
+        for(int cIndex = 0; cIndex < cols; ++cIndex)
+        {
+          Rect tempRect = new Rect((cIndex * gridSize), (rIndex * gridSize), gridSize, gridSize);
+          gridSquares.Children.Add(new RectangleGeometry(tempRect));
+        }
+      }
+
+      GeometryDrawing grid = new GeometryDrawing();
+      grid.Geometry = gridSquares;
+      grid.Pen = new Pen(Brushes.Black, 1);
+
+      DrawingImage geoImage = new DrawingImage(grid);
+      geoImage.Freeze();
+
+      Image image = new Image();
+      image.Source = geoImage;
+
+      MapCanvas.Children.Add(image);
+      // ------------------------------------------------------------------------------------------
+    }
+
+    public void UpdateSysMsg(string msg)
+    {
+      SysMsgBox.Text = msg;
+    }
+
+    #region Menu Methods
+
+    // File
+    private void NewGameClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("New Game Clicked");
+    }
+
+    private void LoadGameClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Load Game Clicked");
+    }
+
+    private void SaveGameClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Save Game Clicked");
+    }
+
+    private void SaveGameAsClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Save Game As Clicked");
+    }
+
+    private void ExitClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Exit Clicked");
+    }
+
+    // Level
+    private void NewLevelClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("New Level Clicked");
+    }
+
+    private void LoadLevelClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Load Level Clicked");
+    }
+
+    private void SaveLevelClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Save Level Clicked");
+    }
+
+    private void SaveLevelAsClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Save Level As Clicked");
+    }
+
+    // Tools
+    private void ImportComponentsClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Import Components Clicked");
+    }
+
+    private void ImportTileSheetClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Import Tile Sheet Clicked");
+    }
+
+    private void ImportTesterClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Import Tester Clicked");
+    }
+
+    private void CreatePrototypeClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Create Prototype Clicked");
+    }
+
+    // View
+    private void ShowConfigClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Show Config Clicked");
+    }
+
+    private void ShowToolsClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Show Tools Clicked");
+    }
+
+    private void ShowHintsClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Show Hints Clicked");
+    }
+
+    private void ShowMessagesClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Show Messages Clicked");
+    }
+
+    private void ShowActionQueuesClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Show Action Queues Clicked");
+    }
+
+    // Test
+    private void TestClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Test Clicked");
+    }
+
+    // Help
+    private void HelpClicked(object sender, RoutedEventArgs e)
+    {
+      UpdateSysMsg("Help Clicked");
+    }
+
+    #endregion
+
+    #region Map Clicks
+
+    private void MapLeftClick(object sender, MouseButtonEventArgs e)
+    {
+      Point loc = e.GetPosition(MapCanvas);
+      Command cmd = Command.CreateMapClickCmd(loc);
+      m_mainEngine.AddCommand(cmd);
+    }
+
+    private void MapRightClick(object sender, MouseButtonEventArgs e)
+    {
+      Point loc = e.GetPosition(MapCanvas);
+      Command cmd = Command.CreateMapClickCmd(loc, false);
+      m_mainEngine.AddCommand(cmd);
+    }
+
+    #endregion
+  }
+}
